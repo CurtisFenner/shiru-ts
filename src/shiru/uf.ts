743eaa3d36c6da13efcd800ffa75c67cd8af752f
Curtis Fenner <3289892+CurtisFenner@users.noreply.github.com>
2025-03-02 14:10:57 +0900

    Implement minimal sound (extremely incomplete) UFTheory: Fail 39 (Slowest 78ms)

--- src/shiru/uf.ts
+++ src/shiru/uf.ts
@@ -1,3 +1,4 @@
+import { TrieMap } from "./data.js";
 import * as ir from "./ir.js";
 import * as sat from "./sat.js";
 import * as smt from "./smt.js";
@@ -8,7 +9,7 @@ export interface UFCounterexample {
 
 export type FnID = symbol & { __brand: "uf.FnID" };
 
-export type ValueID = symbol & { __brand: "uf.FnID" };
+export type ValueID = symbol & { __brand: "uf.ValueID" };
 
 export interface Semantics {
 	/** An `eq` function respects congruence: a == b implies f(a) == f(b). */
@@ -34,12 +35,50 @@ export interface Semantics {
 	interpreter?: (...args: (unknown | null)[]) => unknown | null,
 }
 
+type ValueDefinition = { tag: "application", fn: FnID, operands: ValueID[] }
+	| { tag: "constant", constant: unknown, t: ir.Type }
+	| { tag: "variable", t: ir.Type };
+
 export class UFTheory extends smt.SMTSolver<ValueID[], UFCounterexample> {
+	private applicationTrie = new TrieMap<[FnID, ...ValueID[]], ValueID>();
+
+	private fnMap = new Map<FnID, {
+		returnType: ir.Type,
+		semantics: Semantics,
+	}>();
+
+	private constantMap = new Map<unknown, ValueID>();
+
+	private valueMap = new Map<ValueID, ValueDefinition>();
+
+	private literalMap = new Map<ValueID, sat.Literal>();
+	private termMap = new Map<sat.Literal, ValueID>();
+
+	public readonly trueConstant: ValueID;
+	public readonly falseConstant: ValueID;
+	public readonly notFn: FnID;
+
+	constructor() {
+		super();
+
+		this.trueConstant = this.createConstant(ir.T_BOOLEAN, true);
+		this.falseConstant = this.createConstant(ir.T_BOOLEAN, false);
+		this.notFn = this.createFunction(ir.T_BOOLEAN, {
+			not: true,
+		}, "not");
+
+		this.addConstraint([this.trueConstant]);
+		this.addConstraint([this.createApplication(this.notFn, [this.falseConstant])]);
+	}
+
 	/** For debugging purposes.
 	 *
 	 * Inverts the constraint -> literal mapping implemented by `clausify`.
 	 */
-	override showLiteral(literal: number): string {
+	override showLiteral(literal: sat.Literal): string {
+		if (literal < 0) {
+			return "NOT " + this.showLiteral(-literal);
+		}
 		throw new Error("Method not implemented.");
 	}
 
@@ -48,11 +87,44 @@ export class UFTheory extends smt.SMTSolver<ValueID[], UFCounterexample> {
 		unassigned: sat.Literal[],
 	): { tag: "implied", impliedClauses: sat.Literal[][], model: UFCounterexample }
 		| { tag: "unsatisfiable", conflictClauses: sat.Literal[][] } {
-		throw new Error("TODO: learnTheoryClauses");
+		return {
+			tag: "implied",
+			impliedClauses: [],
+			model: {
+				model: {},
+			},
+		};
 	}
 
 	protected override clausify(constraint: ValueID[]): sat.Literal[][] {
-		throw new Error("TODO: clausify");
+		return [
+			constraint.map(value => this.toSatLiteral(value))
+		];
+	}
+
+	private toSatLiteral(value: ValueID): sat.Literal {
+		const existing = this.literalMap.get(value);
+		if (existing !== undefined) {
+			return existing;
+		}
+
+		const toCache = this.toSatLiteralUncached(value);
+		this.literalMap.set(value, toCache);
+		return toCache;
+	}
+
+	private toSatLiteralUncached(value: ValueID): sat.Literal {
+		const definition = this.valueMap.get(value)!;
+		if (definition.tag === "application") {
+			const fn = this.fnMap.get(definition.fn);
+			if (fn?.semantics.not) {
+				return -this.toSatLiteral(definition.operands[0]);
+			}
+		}
+
+		const nextTerm = this.termMap.size + 1;
+		this.termMap.set(nextTerm, value);
+		return nextTerm;
 	}
 
 	createFunction(
@@ -60,18 +132,51 @@ export class UFTheory extends smt.SMTSolver<ValueID[], UFCounterexample> {
 		semantics: Semantics,
 		debugName: string,
 	): FnID {
-		throw new Error("TODO: createFunction");
+		const fn = Symbol(debugName) as FnID;
+		this.fnMap.set(fn, { returnType, semantics });
+		return fn;
 	}
 
-	createConstant(t: ir.Type, c: unknown): ValueID {
-		throw new Error("TODO: createConstant");
+	createConstant(t: ir.Type, constant: unknown): ValueID {
+		const existing = this.constantMap.get(constant);
+		if (existing !== undefined) {
+			return existing;
+		}
+
+		const value = Symbol(String(constant)) as ValueID;
+		this.constantMap.set(constant, value);
+		this.valueMap.set(value, {
+			tag: "constant",
+			t,
+			constant,
+		});
+		return value;
 	}
 
-	createVariable(type: ir.Type, debugName: string): ValueID {
-		throw new Error("TODO: createVariable");
+	createVariable(t: ir.Type, debugName: string): ValueID {
+		const value = Symbol(debugName) as ValueID;
+		this.valueMap.set(value, {
+			tag: "variable",
+			t,
+		});
+		return value;
 	}
 
-	createApplication(fnID: FnID, args: ValueID[]): ValueID {
-		throw new Error("TODO: createApplication");
+	createApplication(fn: FnID, operands: ValueID[]): ValueID {
+		const key: [FnID, ...ValueID[]] = [fn, ...operands];
+		const existing = this.applicationTrie.get(key);
+		if (existing) {
+			return existing;
+		}
+		const description =
+			(fn.description || "?") + "(" + operands.map(x => x.description || "?").join(", ") + ")";
+		const application = Symbol(description) as ValueID;
+		this.applicationTrie.put(key, application);
+		this.valueMap.set(application, {
+			tag: "application",
+			fn,
+			operands,
+		});
+		return application;
 	}
 }
